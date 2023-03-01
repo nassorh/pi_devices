99419a154b2424ab12eab179bceb217dc80e96a1
nassorh <nassorh.dev@gmail.com>
2023-03-01 15:27:19 +0000

    Button logic

---
+++ button.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+LED = 27 #set ledPin
+button = 18 #set buttonPin
+val = 0 #Button variables
+count = 0 #Record the number of button presses
+flag = 0 #Odd even variable
+
+#SETUP
+GPIO.setmode(GPIO.BCM) # use BCM numbers
+GPIO.setup(button,GPIO.IN,GPIO.PUD_UP) #set the buttonPin INPUT 
+
+while True:
+    val = GPIO.input(button) #Receive button value
+    if(val == 0): #if button is pressed
+        sleep(0.01) #Eliminate button jitter
+        val = GPIO.input(button) #Receive button value
+        if(val == 1): #Loosen the button
+            count = count + 1 #Count the number of clicks on the
+            print("count = %d" %count)
+    flag = count % 2 #Remainder 2 ,Even is 0, odd is 1
+    if(flag == 1):
+        print("Button On")
+    else:
+        print("Button Off")
+
+GPIO.cleanup() #release all GPIO
