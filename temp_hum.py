5cfa328fca630ddea429f249eaca42b453a1978b
nassorh <nassorh.dev@gmail.com>
2023-03-01 21:54:17 +0000

    Changed to 11

--- temp_hum.py
+++ temp_hum.py
@@ -1,7 +1,7 @@
 import Adafruit_DHT
 
 # Set the sensor type and GPIO pin number
-SENSOR_TYPE = Adafruit_DHT.DHT22
+SENSOR_TYPE = Adafruit_DHT.DHT11
 GPIO_PIN = 13
 
 # Loop to read the temperature and humidity values
