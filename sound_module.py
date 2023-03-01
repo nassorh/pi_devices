1b3f4d009de2aca59217870fb9bab9ad380c45cc
nassorh <nassorh.dev@gmail.com>
2023-03-01 16:23:17 +0000

    Sound updated to docs

--- sound_module.py
+++ sound_module.py
@@ -1,31 +1,25 @@
-import spidev
+import RPi.GPIO as GPIO
+import smbus
 import time
 
-# Set the SPI bus and device
-SPI_BUS = 0
-SPI_DEV = 0
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
-# Set the photoresistor channel on the MCP3008
-PHOTO_RESISTOR_CHANNEL = 0
+led = 5
+GPIO.setup(led,GPIO.OUT)
+address = 0x48
+cmd = 0x40
+A0 = 0x40
+A1 = 0x41
+A2 = 0x42
+A3 = 0x43
+bus = smbus.SMBus(1)
 
-# Create an instance of the SPI object
-spi = spidev.SpiDev()
-spi.open(SPI_BUS, SPI_DEV)
+##start the bus
+def analogRead(count): #function, read analog data
+    read_val = bus.read_byte_data(address,cmd + count)
+    return read_val
 
-# Define a function to read the photoresistor value from the MCP3008
-def read_photoresistor():
-    # Send the read command for the photoresistor channel
-    # The first byte is 0b00000001 (start bit, single-ended mode, channel 0)
-    # The second byte is 0b10000000 (null bit, null bit, null bit, null bit, MSB first)
-    resp = spi.xfer2([0b00000001, 0b10000000, 0])
-    
-    # Combine the two bytes of the response into a single value
-    photoresistor_value = (resp[1] << 8) + resp[2]
-    
-    return photoresistor_value
-
-# Loop to read the photoresistor value
 while True:
-    photoresistor_value = read_photoresistor()
-    print("Photoresistor value:", photoresistor_value)
-    time.sleep(0.1)
+    value = analogRead(0) ##read AO data
+    print(value)
