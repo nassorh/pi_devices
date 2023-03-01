1d7ff67959f057a3fc0f27d6c474c5b129132b8e
nassorh <nassorh.dev@gmail.com>
2023-03-01 15:03:15 +0000

    Buzzer sound

---
+++ active_buzzer.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+
+buzzer_pin = 27
+
+# Set the GPIO mode to BCM
+GPIO.setmode(GPIO.BCM)
+# Set up the PIR pin as an input
+GPIO.setup(buzzer_pin, GPIO.IN)
+
+while true:
+    GPIO.output(buzzer_pin,GPIO.HIGH)
+    time.sleep(0.1)
+    GPIO.output(buzzer_pin,GPIO.LOW)
+
+GPIO.cleanup()
