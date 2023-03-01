45027258b37339654584233c79d8abbeba7cfc56
nassorh <nassorh.dev@gmail.com>
2023-03-01 14:31:17 +0000

    AD sound module

--- sound_module.py
+++ sound_module.py
@@ -1,22 +1,29 @@
-import RPi.GPIO as GPIO
-import time
+import smbus
 
-# Define the GPIO pin for the sound sensor
-SOUND_PIN = 23
+# Set the I2C bus number (0 or 1)
+I2C_BUS = 1
 
-# Set up the sound pin as an input
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(SOUND_PIN, GPIO.IN)
+# Set the I2C address of the ADC
+ADC_ADDR = 0x48
 
-# Loop to detect sound
+# Create an instance of the I2C bus object
+i2c = smbus.SMBus(I2C_BUS)
+
+# Define a function to read the ADC value from a given channel
+def read_adc(channel):
+    # Send the channel number to the ADC
+    i2c.write_byte(ADC_ADDR, channel)
+    
+    # Read the ADC value (two bytes, MSB first)
+    msb = i2c.read_byte(ADC_ADDR)
+    lsb = i2c.read_byte(ADC_ADDR)
+    
+    # Combine the two bytes into a single 10-bit value
+    adc_value = (msb << 8) | lsb
+    
+    return adc_value
+
+# Loop to read the ADC value from channel 0
 while True:
-    # Check if sound is detected
-    if GPIO.input(SOUND_PIN):
-        print("Sound detected")
-    else:
-        print("No sound detected")
-    # Wait for a short time to avoid excessive detection
-    time.sleep(0.1)
-
-# Clean up the GPIO pins
-GPIO.cleanup()
+    adc_value = read_adc(0)
+    print("ADC value:", adc_value)
