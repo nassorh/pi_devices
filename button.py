a2a7f49a9fbf5154ae27e4f79d9a17a77239a31f
nassorh <nassorh.dev@gmail.com>
2023-03-01 15:28:27 +0000

    Set pin

--- button.py
+++ button.py
@@ -1,8 +1,7 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-LED = 27 #set ledPin
-button = 18 #set buttonPin
+button = 26 #set buttonPin
 val = 0 #Button variables
 count = 0 #Record the number of button presses
 flag = 0 #Odd even variable
